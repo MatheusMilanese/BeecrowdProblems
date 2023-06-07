feb21f487d6ed4ecae3076413b78ad5af062c058
Matheus Milanese <matheus.milanese@gmail.com>
2023-06-07 16:59:49 -0300

    1001 problem

---
+++ C-Sharp/1001.cs
@@ -0,0 +1,12 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"X = {x+y}");
+    }
+
+}
